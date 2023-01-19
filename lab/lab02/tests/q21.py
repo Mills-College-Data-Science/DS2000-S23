64643b6a1e47b78f21f7fc5164a306820a258693
susanswang <40613331+susanswang@users.noreply.github.com>
2023-01-18 22:51:28 -0800

    Add files via upload

---
+++ lab/lab02/tests/q21.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'q21',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> near_twenty
+          19.99909997918947
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ lab/lab02/tests/q36.py
@@ -0,0 +1,23 @@
+test = {
+  'name': 'q36',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> oakland_markets.num_rows == 10
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
