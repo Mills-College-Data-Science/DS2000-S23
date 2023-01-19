e275fd26ea0adb4da08d687465b8d3bada3a0742
susanswang <40613331+susanswang@users.noreply.github.com>
2023-01-18 22:52:49 -0800

    Add files via upload

---
+++ lab/lab02/tests/q11.py
@@ -0,0 +1,23 @@
+OK_FORMAT = True
+test = {
+  'name': 'q11',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> new_year
+          2023
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
