bec63843cc4e754a7a4738142e7dd48b1fe413ed
susanswang <40613331+susanswang@users.noreply.github.com>
2023-01-19 17:16:03 -0800

    Add files via upload

---
+++ lab/lab02/tests/q0.py
@@ -0,0 +1,23 @@
+OK_FORMAT = True
+test = {
+  'name': 'q0',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> testing
+          1
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
